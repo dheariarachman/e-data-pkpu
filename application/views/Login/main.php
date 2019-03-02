4ae22073c1b92d87b4a8d37e10156dc74d921b9e
Dhea Aria Rachman <dheariarachman@gmail.com>
2019-03-02 19:07:13 +0700

    First Init

---
+++ application/views/Login/main.php
@@ -0,0 +1,54 @@
+<?php $this->load->view('layouts/header'); ?>
+
+<body class="bg-gradient-primary">
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-6 col-lg-6 col-md-6">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
+              <div class="col-lg-12">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                  </div>
+                  <form class="user" action="<?php echo $action; ?>" method="POST">
+                    <div class="form-group">
+                      <input type="text" class="form-control form-control-user" id="username" aria-describedby="usernameHelp" placeholder="Enter Username" name="username">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" name="password">
+                    </div>
+                    <input type="submit" value="Login" class="btn btn-primary btn-user btn-block">
+                    <hr>
+                  </form>
+                  <!-- <div class="text-center">
+                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  </div> -->
+                  <div class="text-center">
+                    <a class="small" href="<?php echo $signup; ?>">Create an Account!</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+    <?php $this->load->view('layouts/footer'); ?>
+
+</body>
+
+</html>
