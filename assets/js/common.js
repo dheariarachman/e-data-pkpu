a8e708f282bafe1591f162b8fb0672ecccbc38e6
Dhea Aria Rachman <dheariarachman@gmail.com>
2019-03-13 13:53:05 +0700

    Update JS Common

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,20 +1,20 @@
 // Indonesia Date's Format
 function formatDate(date) {
-    var monthNames = [
+    let monthNames = [
         "Januari", "Februari", "Maret",
         "April", "Mei", "Juni", "Juli",
         "Agustus", "September", "Oktober",
         "November", "Desember"
     ];
 
-    var daysName = [
+    let daysName = [
         "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"
     ];
 
-    var days        = date.getDay();
-    var day         = date.getDate();
-    var monthIndex  = date.getMonth();
-    var year        = date.getFullYear();
+    let days        = date.getDay();
+    let day         = date.getDate();
+    let monthIndex  = date.getMonth();
+    let year        = date.getFullYear();
 
     return daysName[days] + ', ' + day + ' ' + monthNames[monthIndex] + ' ' + year;
 }
