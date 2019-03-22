ba554d032d1f00ffc881172f8ca162099aff95e3
Dhea Aria Rachman <dheariarachman@gmail.com>
2019-03-22 14:26:56 +0700

    Update ini_set mpdf

--- application/helpers/mpdf_helper.php
+++ application/helpers/mpdf_helper.php
@@ -4,6 +4,7 @@ class mpdf
 {
     public static function print_pdf( $html = '', $orientation = 'P') {
         ini_set("pcre.backtrack_limit", "1000000000");
+        ini_set("memory_limit", "-1");
         $mpdf = new \Mpdf\Mpdf(array('orientation' => $orientation, 'format' => 'A4'));
         $mpdf->writeHTMLHeaders('E-Data - v1');        
         $mpdf->WriteHTML($html);
